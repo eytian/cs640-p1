0f6391acad3ac4c094537a1949887dfe55023c67
eytian <48042255+eytian@users.noreply.github.com>
2022-02-15 16:12:47 -0600

    add client and server classes
    Added EchoClient and EchoServer classes

---
+++ Iperfer.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Iperfer {
+    public static void main(String[] args) {
+        System.out.println("Input: ");
+        
+    }
+}
